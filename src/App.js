2985b05e821f387e7ce14984e5719a4f30b52733
Charizard17 <3723esad@gmail.com>
2020-06-08 20:54:56 +0200

    unnecessary lines deleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from "react";
 import FileReaderComponent from "./FileReaderComponent";
 import { Switch, Route } from "react-router-dom";
-import AddNewProduct from "./Table/AddNewProduct";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myRef: React.createRef(),
-    };
-  }
-
   render() {
     return (
       <div>
         <Switch>
           <Route exact path="/">
             <FileReaderComponent />
-          </Route>
-          <Route exact path="/add">
-            <AddNewProduct />
           </Route>
         </Switch>
       </div>
