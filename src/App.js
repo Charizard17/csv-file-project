ef10c3ac103b53fd15c6a4da503c7af3f6779f93
Charizard17 <3723esad@gmail.com>
2020-06-09 21:03:11 +0200

    unnecessary table component deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import TableComponent from "./Table/TableComponent";
+import "./App.css";
+import Modal from "react-bootstrap/Modal";
 
 class App extends Component {
   constructor(props) {
@@ -8,6 +9,8 @@ class App extends Component {
       fileReader: "",
       headLine: "",
       splittedRows: [],
+      show: false,
+      clickedElement: [],
     };
   }
   handleFileRead = (e) => {
@@ -37,7 +40,7 @@ class App extends Component {
 
   editOnClick = (newElement) => {
     console.log(newElement);
-    console.log(`${this.state.clickedElement}`);
+    console.log(this.state.clickedElement);
   };
 
   render() {
@@ -52,13 +55,301 @@ class App extends Component {
         />
         <div>
           <h2 className="text-center">Table</h2>
-          <TableComponent
-            exact
-            path="/"
-            headLine={this.state.headLine}
-            splittedRows={this.state.splittedRows}
-            editOnClick={this.editOnClick}
-          />
+          <table className="table table-hover table-bordered">
+            <thead>
+              <tr className="grid-container">
+                <th className="Hauptartikelnr" scope="col">
+                  {this.state.headLine[0]}
+                </th>
+                <th className="Artikelname" scope="col">
+                  {this.state.headLine[1]}
+                </th>
+                <th className="Hersteller" scope="col">
+                  {this.state.headLine[2]}
+                </th>
+                <th className="Beschreibung" scope="col">
+                  {this.state.headLine[3]}
+                </th>
+                <th className="Materialangaben" scope="col">
+                  {this.state.headLine[4]}
+                </th>
+                <th className="Geschlecht" scope="col">
+                  {this.state.headLine[5]}
+                </th>
+                <th className="Produktart" scope="col">
+                  {this.state.headLine[6]}
+                </th>
+                <th className="Armel" scope="col">
+                  {this.state.headLine[7]}
+                </th>
+                <th className="Bein" scope="col">
+                  {this.state.headLine[8]}
+                </th>
+                <th className="Kragen" scope="col">
+                  {this.state.headLine[9]}
+                </th>
+                <th className="Herstellung" scope="col">
+                  {this.state.headLine[10]}
+                </th>
+                <th className="Taschenart" scope="col">
+                  {this.state.headLine[11]}
+                </th>
+                <th className="Grammatur" scope="col">
+                  {this.state.headLine[12]}
+                </th>
+                <th className="Material" scope="col">
+                  {this.state.headLine[13]}
+                </th>
+                <th className="Ursprungsland" scope="col">
+                  {this.state.headLine[14]}
+                </th>
+                <th className="Bildname" scope="col">
+                  {this.state.headLine[15]}
+                </th>
+                <th className="Edit" scope="col">
+                  Edit
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.splittedRows.map((element, i) => {
+                return (
+                  <tr key={i} className="grid-container">
+                    <th className="Hauptartikelnr" scope="row">
+                      {element[0]}
+                    </th>
+                    <td className="Artikelname">{element[1]}</td>
+                    <td className="Hersteller">{element[2]}</td>
+                    <td className="Beschreibung">{element[3]}</td>
+                    <td className="Materialangaben">{element[4]}</td>
+                    <td className="Geschlecht">{element[5]}</td>
+                    <td className="Produktart">{element[6]}</td>
+                    <td className="Armel">{element[7]}</td>
+                    <td className="Bein">{element[8]}</td>
+                    <td className="Kragen">{element[9]}</td>
+                    <td className="Herstellung">{element[10]}</td>
+                    <td className="Taschenart">{element[11]}</td>
+                    <td className="Grammatur">{element[12]}</td>
+                    <td className="Material">{element[13]}</td>
+                    <td className="Ursprungsland">{element[14]}</td>
+                    <td className="Bildname">{element[15]}.jpg</td>
+                    <td className="Edit">
+                      <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={() => {
+                          this.setState({
+                            clickedElement: element,
+                            show: true,
+                          });
+                        }}
+                      >
+                        +
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Modal
+            show={this.state.show}
+            onHide={() => this.setState({ show: false })}
+            dialogClassName="modal-90w"
+            aria-labelledby="example-custom-modal-styling-title"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="example-custom-modal-styling-title">
+                Element Edit
+              </Modal.Title>
+              clickedElement
+            </Modal.Header>
+            <Modal.Body>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Hauptartikelnr
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[0]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Artikelname
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[1]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Hersteller
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[2]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Beschreibung
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[3]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Materialangaben
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[4]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Geschlecht
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[5]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Produktart
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[6]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Armel
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[7]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Bein
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[8]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Kragen
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[9]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Herstellung
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[10]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Taschenart
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[11]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Grammatur
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[12]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Material
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[13]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Ursprungsland
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[14]}`}
+                ></textarea>
+              </div>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" style={{ width: "150px" }}>
+                    Bildname
+                  </span>
+                </div>
+                <textarea
+                  className="form-control"
+                  defaultValue={`${this.state.clickedElement[15]}`}
+                ></textarea>
+              </div>
+            </Modal.Body>
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={this.editOnClick}
+            >
+              Edit
+            </button>
+          </Modal>
         </div>
       </div>
     );
